a12a162dedcb38acf46fe1df638ca5b7744ee5b2
Priyanshu0931 <priyanshujha2002@gmail.com>
2024-09-15 02:21:13 +0530

    minor fix

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,15 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
+# Base class for all models
 Base = declarative_base()
 
+# User model
 class User(Base):
     __tablename__ = 'users'
-
+    # user_id is the primary key
     user_id = Column(String(255), primary_key=True, index=True)
     api_calls = Column(Integer, default=1)
-
+    # __repr__ method to represent the object
     def __repr__(self):
         return f"<User(user_id={self.user_id}, api_calls={self.api_calls})>"
